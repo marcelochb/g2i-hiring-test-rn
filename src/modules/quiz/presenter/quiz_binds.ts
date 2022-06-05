b925a6752fd545e865d7242a6b3f33ee1003d676
Marcelo Block Teixeira <marcelochb@gmail.com>
2022-06-05 18:23:48 -0300

    bind

---
+++ src/modules/quiz/presenter/quiz_binds.ts
@@ -0,0 +1,13 @@
+import { InjectContants } from "@src/core/constants"
+import { ApiClient } from "@src/core/services"
+import { container } from "tsyringe"
+import { GetAllQuizUseCase } from "../domain/usecases/get_all_quiz_usecase";
+import { QuizDatasource } from "../external/datasources/quiz_datasource";
+import { QuizRepository } from "../infra/repositories";
+
+export const QuizDependences = () => {
+  container.register(InjectContants.IApiClient,{useValue: new ApiClient()});
+  container.register(InjectContants.IQuizDatasource,{useClass: QuizDatasource});
+  container.register(InjectContants.IQuizRepository,{useClass: QuizRepository});
+  container.register(InjectContants.GetAllQuizUseCase,{useClass: GetAllQuizUseCase});
+}
