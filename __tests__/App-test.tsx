97168fbd8413ac58bf1514de380c8a0cddb9d513
Marcelo Block Teixeira <marcelochb@gmail.com>
2022-06-11 11:33:14 -0300

    Config unit test - jest

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -1,10 +1,10 @@
 /**
  * @format
  */
-
+import 'reflect-metadata';
 import 'react-native';
 import React from 'react';
-import App from '../App';
+import App from '../src';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
